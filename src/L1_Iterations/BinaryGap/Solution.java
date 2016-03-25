109ed887711a62d80f47125666be9e7a67ffedf9
Sumeet Das <sumeetdas1992@gmail.com>
2016-03-25 07:26:20 +0530

    initial commit; adds all new Solution and README files

---
+++ src/L1_Iterations/BinaryGap/Solution.java
@@ -0,0 +1,24 @@
+package L1_Iterations.BinaryGap;
+/**
+ * Created by sumedas on 25-Mar-16.
+ */
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+
+        final BufferedOutputStream outputStream = new BufferedOutputStream(System.out);
+
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        final StringTokenizer stringTokenizer = new StringTokenizer(reader.readLine());
+
+        outputStream.flush();
+    }
+}
